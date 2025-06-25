91097c166351ffca8e8c10b04ab89aad52d1c9c5
Mr-shubham1 <shubhamguptajnvc@gmail.com>
2025-06-25 10:50:44 +0530

    font end initiallized

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="">
+      <h1>hello</h1>
+      <button className="btn btn-secondary">Secondary</button>
+    </div>
+  );
+}
+
+export default App;
